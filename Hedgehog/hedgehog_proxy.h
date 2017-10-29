e411afb5768dd29bc08fd102f8b9a782f198da26
eaniconer <eaniconer@gmail.com>
2017-10-29 21:53:07 +0300

    Getting coordinates of hedgehog

--- Hedgehog/hedgehog_proxy.h
+++ Hedgehog/hedgehog_proxy.h
@@ -1,9 +1,28 @@
+#pragma once
 #ifndef HEDGEHOG_PROXY_H
 #define HEDGEHOG_PROXY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <unistd.h>
+#include <signal.h>
+
+#include <stdio.h>
+#include "marvelmind.h"
+
 class HedgehogProxy
 {
- 
+public:
+    HedgehogProxy();
+    ~HedgehogProxy();
+    PositionValue get_data();
+
+    bool terminationRequired() { return hedge->terminationRequired; }
+private:
+    struct MarvelmindHedge* hedge;
 };
 
 #endif // HEDGEHOG_PROXY_H
+
